20d633d6c88a380be47180f9e90474c1d79cb74c
iansicking <ian.sicking@cccareers.org>
2026-03-30 14:52:21 +0100

    San Diego Top Spots

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-$(document).ready(function () {
-    // write your code here
+$(document).ready(function ()
+{
+    $.getJSON("data.json", function (data)
+    {
+        let rows = "";
+        $.each(data, function (i, site)
+        {
+            rows += `
+                <tr>
+                    <td>${site.name}</td>
+                    <td>${site.description}</td>
+                    <td><a href="https://www.google.com/maps?q=${site.location}" target="_blank" class="button-style">Open in Google Maps</a></td>
+                </tr>
+            `;
+        });
 
+        $("#data-table").html(rows);
+    })
 });
+
+
